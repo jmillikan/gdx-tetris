14e4e1829cf756cf6af5493be349e1d39e232697
Jesse Millikan <jesse.a.millikan@gmail.com>
2014-03-31 17:54:58 -0400

    'works', lines clear and game over occurs if new piece overlaps a block on the field. Unused pause buttons on the graphic.

--- gdx-tetris/src/com/highestqualitygames/gdx_tetris/Assets.java
+++ gdx-tetris/src/com/highestqualitygames/gdx_tetris/Assets.java
@@ -13,9 +13,8 @@ public class Assets {
 	// These rectangles are relative to the BG (and so will need adjusted if the camera is not 1:1 with it)
 	public static Rectangle mainScreenStart;
 	
-	public static Rectangle gameScreenRotLeft, gameScreenRotRight, gameScreenLeft, gameScreenRight;
-	
-	public static Rectangle gameScreenGrid;
+	public static Rectangle gameScreenRotLeft, gameScreenRotRight, gameScreenLeft, gameScreenRight,
+		gameScreenGrid, gameScreenDrop, gameScreenPause1, gameScreenPause2;
 	
 	public static void load() {
 		Texture texture;
@@ -23,12 +22,17 @@ public class Assets {
 		texture = new Texture(Gdx.files.internal("data/game-screen.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
+		// From game-screen.svg - x is SVG x, y is SVG y - 32 (y points up)
+		// Width and height are 1:1 for now
 		gameScreen = new TextureRegion(texture, 0, 0, 800, 480);
-		gameScreenRotLeft = new Rectangle(123, 200, 138, 143); 
-		gameScreenRotRight = new Rectangle(542, 200, 138, 143); 
+		gameScreenRotLeft = new Rectangle(103, 200, 138, 143); 
+		gameScreenRotRight = new Rectangle(562, 200, 138, 143); 
 		gameScreenLeft = new Rectangle(123, 32, 138, 143); 
 		gameScreenRight = new Rectangle(542, 32, 138, 143); 
 		gameScreenGrid = new Rectangle(300, 39, 10 * 20, 22 * 20);
+		gameScreenDrop = new Rectangle(277, 10, 243, 94);
+		gameScreenPause1 = new Rectangle(102, 412, 138, 78);
+		gameScreenPause2 = new Rectangle(562, 412, 138, 78);
 		
 		texture = new Texture(Gdx.files.internal("data/main-screen.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
--- gdx-tetris/src/com/highestqualitygames/gdx_tetris/GameScreen.java
+++ gdx-tetris/src/com/highestqualitygames/gdx_tetris/GameScreen.java
@@ -12,26 +12,31 @@ import com.badlogic.gdx.math.Vector3;
 public class GameScreen implements Screen {
 	SpriteBatch batch = new SpriteBatch();
 	Vector3 touchPoint = new Vector3();
+
+	Game game;
+	OrthographicCamera cam;
+	Sprite mm_sprite, block_sprite;
+	
 	final int BLOCK_SIZE = 20;
-	final int GRID_HEIGHT = 22;
-	final int GRID_WIDTH = 10;
+	
 	final float DROP_TIMEOUT = 1;
 	
 	// row-major grid, 0 at the bottom, 21/20 hidden at the top
-	
-	// Currently blocks are 20px, rendered from (300,39) "up"
+	final int GRID_HEIGHT = 22;
+	final int GRID_WIDTH = 10;
+
 	boolean[][] grid = new boolean[GRID_HEIGHT][GRID_WIDTH];
 	
 	// A square array of length 2/3/4
 	boolean[][] piece;
-	// These are interpreted as grid position of the top left corner.
+	
+	// These are interpreted as grid position of the top left corner
+	// of the piece (which give the odd-looking "- i" bits) -
+	// should be changed to bottom left corner at some point
 	int piece_x;
 	int piece_y;
-	float next_drop;
 	
-	Game game;
-	OrthographicCamera cam;
-	Sprite mm_sprite, block_sprite;
+	float next_drop;
 	
 	public GameScreen(Game game) {
 		this.game = game;
@@ -55,25 +60,22 @@ public class GameScreen implements Screen {
 		next_drop -= delta;
 		
 		if (next_drop <= 0) {
-			if(!collidesWithGridOrWall(piece, piece_x, piece_y - 1)){
-				piece_y = piece_y - 1;
-				
-				next_drop = DROP_TIMEOUT - next_drop;
-			}
-			else {
-				attachPiece();
-				// Attach, new piece...
-				spawnPiece();
-				
-				if(collidesWithGridOrWall(piece, piece_x, piece_y)){
-					gameOver();
-				}
-			}
+			dropPiece(false);
 		}
 		
+		handleGameKeys();
+		
+		drawGame();
+	}
+	
+	public void handleGameKeys() {
 		if (Gdx.input.justTouched()) {
 			cam.unproject(touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0));
-
+			
+			if (Assets.gameScreenDrop.contains(touchPoint.x, touchPoint.y)) {
+				dropPiece(true);
+			}
+			
 			if (Assets.gameScreenLeft.contains(touchPoint.x, touchPoint.y)) {
 				if(!collidesWithGridOrWall(piece, piece_x - 1, piece_y)){
 					piece_x -= 1;
@@ -87,63 +89,27 @@ public class GameScreen implements Screen {
 			}
 			
 			if (Assets.gameScreenRotLeft.contains(touchPoint.x, touchPoint.y)) {
-				boolean[][] new_piece = this.rotate(true, piece);
+				boolean[][] new_piece = this.rotate(false, piece);
 				if(!collidesWithGridOrWall(new_piece, piece_x, piece_y)){
 					piece = new_piece;
 				}
 			}
 			
 			if (Assets.gameScreenRotRight.contains(touchPoint.x, touchPoint.y)) {
-				boolean[][] new_piece = this.rotate(false, piece);
+				boolean[][] new_piece = this.rotate(true, piece);
 				if(!collidesWithGridOrWall(new_piece, piece_x, piece_y)){
 					piece = new_piece;
 				}
 			}
 		}
-		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(cam.combined);
-		
-		batch.begin();
-		mm_sprite.draw(batch);
-		batch.end();
-		
-		batch.begin();
-		
-		float grid_x = Assets.gameScreenGrid.x;
-		float grid_y = Assets.gameScreenGrid.y;
-		
-		for(int i = 0; i < 20; i++){
-			for(int j = 0; j < 10; j++){
-				if(grid[i][j])
-					batch.draw(	
-							block_sprite, 
-							grid_x + j * BLOCK_SIZE,
-							grid_y + i * BLOCK_SIZE);
-			}
-		}
-		
-		int size = piece.length;
-		for(int i = 0; i < size; i++){
-			for(int j = 0; j < size; j++){
-				if(piece[i][j]){
-					batch.draw(
-							block_sprite, 
-							grid_x + BLOCK_SIZE * (piece_x + j),
-							grid_y + BLOCK_SIZE * (piece_y - i));
-				}
-			}
-		}
-		
-		batch.end();
 	}
 	
 	public void spawnPiece(){
 		this.piece = randomPiece();
 		this.piece_x = 3;
-		this.piece_y = 20;	
+		this.piece_y = 21;	
+		
+		next_drop = DROP_TIMEOUT;
 	}
 	
 	public void attachPiece(){
@@ -153,6 +119,50 @@ public class GameScreen implements Screen {
 				if(piece[i][j]){
 					grid[piece_y - i][piece_x + j] = true;
 				}
+			}
+		}
+	}
+	
+	public void dropPiece(boolean byUser){
+		if(!collidesWithGridOrWall(piece, piece_x, piece_y - 1)){
+			piece_y = piece_y - 1;
+			
+			next_drop = DROP_TIMEOUT - (byUser ? 0 : next_drop);
+		}
+		else {
+			// TODO: Pause game for this...
+			attachPiece();
+			
+			clearRows();
+			
+			spawnPiece();
+			
+			if(collidesWithGridOrWall(piece, piece_x, piece_y)){
+				gameOver();
+			}
+		}
+	}
+	
+	public void clearRows(){
+		for(int i = 0; i < GRID_HEIGHT;){
+			boolean full = true;
+			
+			for(int j = 0; j < GRID_WIDTH; j++) {
+				if(!grid[i][j]) 
+					full = false;
+			}
+			
+			if(full){
+				// Move rows down...
+
+				for(int k = i; k < GRID_HEIGHT - 1; k++){
+					grid[k] = grid[k + 1];
+				}
+				
+				grid[GRID_HEIGHT - 1] = new boolean[GRID_WIDTH];
+			}
+			else {
+				i++;
 			}
 		}
 	}
@@ -178,6 +188,47 @@ public class GameScreen implements Screen {
 		}
 		
 		return false;
+	}
+	
+	public void drawGame(){
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		batch.setProjectionMatrix(cam.combined);
+		
+		batch.begin();
+		mm_sprite.draw(batch);
+		batch.end();
+		
+		batch.begin();
+		
+		float grid_x = Assets.gameScreenGrid.x;
+		float grid_y = Assets.gameScreenGrid.y;
+		
+		// Render all but top two rows
+		for(int i = 0; i < GRID_HEIGHT - 2; i++){
+			for(int j = 0; j < GRID_WIDTH; j++){
+				if(grid[i][j])
+					batch.draw(	
+							block_sprite, 
+							grid_x + j * BLOCK_SIZE,
+							grid_y + i * BLOCK_SIZE);
+			}
+		}
+		
+		int size = piece.length;
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				if(piece[i][j] && piece_y - i < GRID_HEIGHT - 2){
+					batch.draw(
+							block_sprite, 
+							grid_x + BLOCK_SIZE * (piece_x + j),
+							grid_y + BLOCK_SIZE * (piece_y - i));
+				}
+			}
+		}
+		
+		batch.end();
 	}
 
 	@Override
@@ -234,13 +285,13 @@ public class GameScreen implements Screen {
 		return pieces[(int) Math.floor(Math.random() * 7)];
 	}
 	
-	private boolean[][] rotate(boolean left, boolean[][] piece){
+	private boolean[][] rotate(boolean right, boolean[][] piece){
 		// Assume that piece is a square (not ragged or rectangular)
 		boolean[][] new_piece = new boolean[piece.length][piece[0].length];
 		
 		for(int i = 0; i < piece.length; i++){
 			for(int j = 0; j < piece.length; j++){
-				new_piece[left ? j : piece.length - 1 - j][left ? piece.length - 1 - i : i] = piece[i][j]; 
+				new_piece[right ? j : piece.length - 1 - j][right ? piece.length - 1 - i : i] = piece[i][j]; 
 			}
 		}
 		
